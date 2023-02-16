d2d056068999ca9541e98e31c0f286fe152333b3
jinsu <realwaterkim@datacommand.kr>
2023-02-17 08:48:58 +0900

    usecase 변경

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"timetable-go/adapter"
+	"timetable-go/infra/repository"
 	"timetable-go/infra/rest"
 )
 
 func main() {
-	uc := adapter.NewSimpleUsecase()
+	uc := adapter.NewSimpleUsecase(repository.NewMemoryRepository())
 	err := rest.NewRest(uc).Run()
 	log.Println(err)
 }
